d0441cdf097b6ed34a0f5ec0f70aac3eb83f313c
ravneet0628 <ravneet0628@gmail.com>
2025-04-15 00:15:20 -0400

    updated API url

--- frontend/src/AdminPage.js
+++ frontend/src/AdminPage.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
-const API_URL = process.env.API_URL || "http://localhost:5000";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
 
 
 function AdminPage() {
--- frontend/src/ContactForm.js
+++ frontend/src/ContactForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-const API_URL = process.env.API_URL || "http://localhost:5000";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
 
 
 function ContactForm() {
